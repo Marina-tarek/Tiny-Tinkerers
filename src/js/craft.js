2573a90619404f1e73eb4e41af7b7ed9deb049ba
Marina-tarek <marina.tarek.w@gmail.com>
2025-08-26 08:54:01 +0300

    update Crafts page

--- src/js/craft.js
+++ src/js/craft.js
@@ -1,8 +1,23 @@
-let craftList=[];
-
-let craftData = localStorage.getItem("crafts-idea");
-craftList.push(craftData)
-console.log(craftList);
+let CraftList = JSON.parse(localStorage.getItem('crafts-idea')) || [];
+console.log(CraftList);
 
 
+function displayAllCraft(){
+  cartonaa='';
+  for (let z = 0; z< CraftList.length; z++) {
+   cartonaa+=`<div class="craft_card bg-white p-6 rounded-2xl shadow-lg hover:scale-105 hover:bg-[#EE4F31] hover:text-white transition text-center w-7/12  md:w-5/12 lg:w-[26%] xl:w-[22%] relative">
+            <img src="${CraftList[z].main_image}" alt="${CraftList[z].name}" class="rounded-xl  w-full h-[15rem] object-fill">
+            <div class="card-title-bar  bg-[#EE4F31] hover:bg-white hover:text-[#EE4F31] absolute top-[50%]">
+              <h3 class="text-2xl font-semibold font-serif">${CraftList[z].name}</h3>
+            </div>
+            <p class="text-gray-600 mt-10">${CraftList[z].description.split(" ").slice(0,20).join(" ")}</p>
+          <div class="mt-10">
+          <a href="craft.html?id=${CraftList[z].id}" class="btn text-xl bg-[#00C2D7]  py-2 px-2.5 rounded-4xl text-white hover:bg-white hover:text-[#00C2D7] hover:border-[#00C2D7] transition-all">Show This Craft</a></div>
+        </div>
+          </div>`
+    
+  }
+  document.getElementById("All_craft_cards").innerHTML=cartonaa
+}
 
+displayAllCraft()
--- src/js/index.js
+++ src/js/index.js
@@ -30,20 +30,21 @@ let List = JSON.parse(localStorage.getItem('crafts-idea')) || [];
 
 function displaySomeCraft(){
   cartona='';
-  for (let i = 0; i < List.length; i++) {
+  for (let i = 0; i < 8; i++) {
    cartona+=`<div class="craft_card bg-white p-6 rounded-2xl shadow-lg hover:scale-105 hover:bg-[#EE4F31] hover:text-white transition text-center w-7/12  md:w-5/12 lg:w-4/12 xl:w-[22%] relative">
-            <img src="${List[i].main_image}" alt="Clay Model" class="rounded-xl  w-full h-[15rem] object-fill">
+            <img src="${List[i].main_image}" alt="${List[i].name}" class="rounded-xl  w-full h-[15rem] object-fill">
             <div class="card-title-bar  bg-[#EE4F31] hover:bg-white hover:text-[#EE4F31] absolute top-[50%]">
               <h3 class="text-2xl font-semibold font-serif">${List[i].name}</h3>
             </div>
-            <p class="text-gray-600 mt-10">${List[i].description.split(" ").slice(1,20).join(" ")}</p>
+            <p class="text-gray-600 mt-10">${List[i].description.split(" ").slice(0,20).join(" ")}</p>
           <div class="mt-10">
-          <a href="craft.html" class="btn bg-[#00C2D7]  py-2 px-2.5 rounded-4xl text-white hover:bg-white hover:text-[#00C2D7] hover:border-[#00C2D7] transition-all">Show This Craft</a></div>
+          <a href="craft.html" class="btn text-xl bg-[#00C2D7]  py-2 px-2.5 rounded-4xl text-white hover:bg-white hover:text-[#00C2D7] hover:border-[#00C2D7] transition-all">Show This Craft</a></div>
         </div>
           </div>`
     
   }
   document.getElementById("popular_craft_cards").innerHTML=cartona
+
 
 }
 
