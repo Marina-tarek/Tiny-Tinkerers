2d5534d5b9f0f74bd49af68c2aa8bd5bc744590d
Marina-tarek <marina.tarek.w@gmail.com>
2025-08-27 14:57:53 +0300

    small edit

--- dist/js/craft.js
+++ dist/js/craft.js
@@ -7,10 +7,10 @@ function displayAllCraft(){
   for (let z = 0; z< CraftList.length; z++) {
    cartonaa+=`<div class="craft_card bg-white p-6 rounded-2xl shadow-lg hover:scale-105 hover:bg-[#EE4F31] hover:text-white transition text-center w-7/12  md:w-5/12 lg:w-[26%] xl:w-[22%] relative">
             <img src="${CraftList[z].main_image}" alt="${CraftList[z].name}" class="rounded-xl  w-full h-[15rem] object-fill">
-            <div class="card-title-bar text-center py-2.5 px-5  bg-[#EE4F31] hover:bg-white hover:text-[#EE4F31] absolute top-[50%]">
+            <div class="card-title-bar text-center py-2.5 px-5  bg-[#EE4F31] hover:bg-white hover:text-[#EE4F31] absolute top-[50%] transition">
               <h3 class="text-2xl font-semibold font-serif">${CraftList[z].name}</h3>
             </div>
-            <p class="text-gray-600 mt-10">${CraftList[z].description.split(" ").slice(0,15).join(" ")}</p>
+            <p class=" mt-10 transition">${CraftList[z].description.split(" ").slice(0,15).join(" ")}</p>
           <div class="mt-10">
           <a href="craft_desc.html?id=${CraftList[z].id}" class="btn text-xl bg-[#00C2D7]  py-2.5 px-5 rounded-4xl text-white hover:bg-white hover:text-[#00C2D7] hover:border-[#00C2D7] transition-all">Show This Craft</a></div>
         </div>
