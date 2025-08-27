96a5a724d63a255e1e6ecbbe62049611ffaa1d0e
Marina-tarek <marina.tarek.w@gmail.com>
2025-08-27 18:12:09 +0300

    final edit

--- dist/js/craft.js
+++ dist/js/craft.js
@@ -7,8 +7,8 @@ function displayAllCraft(){
   for (let z = 0; z< CraftList.length; z++) {
    cartonaa+=`<div class="craft_card bg-white p-6 rounded-2xl shadow-lg hover:scale-105 hover:bg-[#EE4F31] hover:text-white transition text-center w-7/12  md:w-5/12 lg:w-[26%] xl:w-[22%] relative">
             <img src="${CraftList[z].main_image}" alt="${CraftList[z].name}" class="rounded-xl  w-full h-[15rem] object-fill">
-            <div class="card-title-bar text-center py-2.5 px-5  bg-[#EE4F31] hover:bg-white hover:text-[#EE4F31] absolute top-[50%] transition">
-              <h3 class="text-2xl font-semibold font-serif">${CraftList[z].name}</h3>
+            <div class="card-title-bar right-[-5px] left-[-5px] text-center py-2.5 px-5  bg-[#EE4F31] hover:bg-white hover:text-[#EE4F31] absolute top-[50%] transition">
+              <h3 class="text-2xl font-semibold font-serif m-0">${CraftList[z].name}</h3>
             </div>
             <p class=" mt-10 transition">${CraftList[z].description.split(" ").slice(0,15).join(" ")}</p>
           <div class="mt-10">
